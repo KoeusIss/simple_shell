b38d125f53632f14f6625e16e0b060b3a64601d0
KoeusIss <sebri.issam@gmail.com>
2020-04-05 19:33:21 +0100

    Commit the header file

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* Header libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+/* Prototypes */
+
+
+#endif /* SHELL_H */
